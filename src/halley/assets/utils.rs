74b104369ad3eae3ad1ddaf780b62ab8230d2b6f
Gadi Piperno Corcos <gadi@skyriseltd.com>
2023-11-15 01:17:48 +0100

    :sparkles: Added unpack utils and functions

--- src/halley/assets/utils.rs
+++ src/halley/assets/utils.rs
@@ -1,3 +1,8 @@
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
 pub fn pathify(name: &str, u_ext: &str) -> String {
     let filename = if !name.contains('.') {
         format!("{}{}", name, u_ext).to_string()
@@ -14,4 +19,31 @@ pub fn unpathify(name: &str, u_ext: &str) -> String {
         filename = filename[0..filename.len() - u_ext.len()].to_string();
     }
     filename
+}
+
+pub fn get_dat_files(dir: &Path) -> Vec<PathBuf> {
+    let mut files = Vec::new();
+    for entry in fs::read_dir(dir).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_file() && path.extension().unwrap() == "dat" {
+            files.push(path);
+        }
+    }
+    files
+}
+
+pub fn copy_assets(src: &Path, dst: &Path, force: Option<bool>) {
+    let dat_files = get_dat_files(src);
+    if !dst.exists() && !dat_files.is_empty() {
+        fs::create_dir_all(dst).unwrap();
+    }
+    let force = force.unwrap_or(false);
+    for dat_file in dat_files {
+        let filename = dat_file.file_name().unwrap().to_str().unwrap();
+        let dst_file = dst.join(filename);
+        if force || !dst_file.exists() {
+            fs::copy(dat_file, dst_file).unwrap();
+        }
+    }
 }
--- src/halley/mod.rs
+++ src/halley/mod.rs
@@ -1,2 +1,57 @@
+use std::{
+    fs,
+    io::{BufWriter, Write},
+    path::Path,
+};
+
+use cookie_factory::WriteContext;
+use serde::{Deserialize, Serialize};
+
+use self::{
+    assets::{unpack::pack_halley_pk, utils::get_dat_files},
+    versions::{common::hpk::HalleyPack, v2020::hpk::HpkSectionV2020, v2023::hpk::HpkSectionV2023},
+};
+
 pub mod assets;
 pub mod versions;
+
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+pub enum PackVersion {
+    V2020,
+    V2023,
+}
+
+pub fn unpack_assets(src: &Path, dst: &Path, pack_version: PackVersion) {
+    let dat_files = get_dat_files(src);
+    if !dst.exists() && !dat_files.is_empty() {
+        fs::create_dir_all(dst).unwrap();
+    }
+    for dat_file in dat_files {
+        let filename = dat_file.file_name().unwrap().to_str().unwrap();
+        let dst_file = dst.join(filename);
+        if dst_file.exists() {
+            fs::remove_dir_all(&dst_file).unwrap();
+        }
+        fs::create_dir_all(&dst_file).unwrap();
+        let pack = unpack_asset(&dat_file, pack_version);
+        write_pack(pack, &dst_file);
+    }
+}
+
+pub fn unpack_asset(path: &Path, pack_version: PackVersion) -> Box<dyn HalleyPack> {
+    match pack_version {
+        PackVersion::V2023 => pack_halley_pk::<HpkSectionV2023>(path).unwrap(),
+        PackVersion::V2020 => pack_halley_pk::<HpkSectionV2020>(path).unwrap(),
+    }
+}
+
+pub fn write_pack(pack: Box<dyn HalleyPack>, path: &Path) {
+    let mut writer = BufWriter::new(fs::File::create(path).unwrap());
+    let buf = vec![];
+    let res = pack.write()(WriteContext {
+        write: buf,
+        position: 0,
+    })
+    .unwrap();
+    writer.write_all(&res.write).unwrap();
+}
